e9c27e8140b497846c0cb8824806905396f367d4
Abderrahim Dahmani <80603216+AbdrrahimDahmani@users.noreply.github.com>
2023-05-31 17:24:03 +0100

    admin pannel updated

--- SystemAdminView/sideNavBar.php
+++ SystemAdminView/sideNavBar.php
@@ -12,31 +12,35 @@
 <body>
     <div class="row flex-nowrap">
         <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100">
                 <a href="systemAdminPortalHome.php" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                     <span class="fs-5 d-none d-sm-inline">Menu</span>
                 </a>
                 <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
                     <li>
-                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                        <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-table" viewBox="0 0 16 16">
                                 <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 2h-4v3h4V4zm0 4h-4v3h4V8zm0 4h-4v3h3a1 1 0 0 0 1-1v-2zm-5 3v-3H6v3h4zm-5 0v-3H1v2a1 1 0 0 0 1 1h3zm-4-4h4V8H1v3zm0-4h4V4H1v3zm5-3v3h4V4H6zm4 4H6v3h4V8z" />
                             </svg>
-                            <span class="ms-1 d-none d-sm-inline">Tables</span> </a>
+                            <span class="ms-1 d-none d-sm-inline">View</span> </a>
                         <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
                             <li class="w-100">
-                                <a href="usersTableEditDelete.php" class="nav-link px-0"> <span class="d-none d-sm-inline">Users</span>
+                                <a href="usersTableEditDelete.php" class="nav-link px-0 text-white">
+                                     <span class="d-none d-sm-inline">Users</span>
                                 </a>
                             </li>
                             <li class="w-100">
-                                <a href="patientsTableEditDelete.php" class="nav-link px-0"> <span class="d-none d-sm-inline">Patients</span>
+                                <a href="patientsTableEditDelete.php" class="nav-link px-0 text-white"> 
+                                    <span class="d-none d-sm-inline">Patients</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="doctorsTableEditDelete.php" class="nav-link px-0"> <span class="d-none d-sm-inline">Doctors</span></a>
+                                <a href="doctorsTableEditDelete.php" class="nav-link px-0 text-white"> 
+                                    <span class="d-none d-sm-inline">Doctors</span></a>
                             </li>
                             <li>
-                                <a href="adminsTableEditDelete.php" class="nav-link px-0"> <span class="d-none d-sm-inline">Admins</span>
+                                <a href="adminsTableEditDelete.php" class="nav-link px-0 text-white">
+                                     <span class="d-none d-sm-inline">Admins</span>
                                 </a>
                             </li>
                         </ul>
@@ -45,7 +49,7 @@
                         &ensp;
                     </li>
                     <li>
-                        <a href="newDoctor.php" class="nav-link px-0 align-middle">
+                        <a href="newDoctor.php" class="nav-link px-0 align-middle text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
                                 <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                                 <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
@@ -56,7 +60,7 @@
                         &ensp;
                     </li>
                     <li>
-                        <a href="search.php" class="nav-link px-0 align-middle">
+                        <a href="search.php" class="nav-link px-0 align-middle text-white">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                             </svg>
--- SystemAdminView/systemAdminPortalHome.php
+++ SystemAdminView/systemAdminPortalHome.php
@@ -1,46 +1,97 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="../style.css">
-</head>
 
-<body>
+    <link rel="stylesheet" href="../style.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+    />
+    <link rel="stylesheet" href="../style/admin.css" />
+  </head>
+
+  <body>
     <?php include "../checkSignedIn.php" ?>
     <div class="container-fluid">
-        <?php include "sideNavBar.php" ?>
+      <?php include "sideNavBar.php" ?>
+      <section class="dashboard">
+        <div class="dash-content">
+          <div class="overview">
+            <div class="title">
+              <i class="fa-solid fa-chart-simple"></i>
+              <span class="text">Overview</span>
+            </div>
 
-        <!-- this is where the views go?? -->
-        <div class="col py-3">
-            <h3>Welcome System Administrator!</h3>
+            <div class="boxes">
+              <div class="box box1">
+                <i class="fa-solid fa-user-tie"></i>
+                <span class="text">Total Doctors</span>
+                <span class="number">10</span>
+              </div>
+              <div class="box box2">
+                <i class="fa-solid fa-people-group"></i>
+                <span class="text">Total Patient</span>
+                <span class="number">50</span>
+              </div>
+              <div class="box box3">
+                <i class="fa-solid fa-scale-balanced"></i>
+                <span class="text">Total Appointments</span>
+                <span class="number">25</span>
+              </div>
+            </div>
+          </div>
 
-            <p>
-                To edit, search, or delete entries in the tables, click on the appropriate navigation on the left hand side under 
-                the Tables tab. <br>
-                This is where you can edit the appropriate data for each user by type.
+          <div class="activity">
+            <div class="title">
+              <i class="fa-solid fa-clock-rotate-left"></i>
+              <span class="text">Recent Sign Up</span>
+            </div>
 
-                <br><br>
-                If you would like to <b>create a Doctor account</b> click the New Doctor tab.
-
-                <br><br>
-                To search user by name/username/ID click on the search tab.
-            </p>
-
-            <h3 class="col py-3">Happy Editing :)</h3>
-
+            <div class="activity-data">
+              <div class="data">
+                <span class="data-title">Name</span>
+                <span class="data-list">Prem Shahi</span>
+                <span class="data-list">Deepa Chand</span>
+                <span class="data-list">Manisha Chand</span>
+                <span class="data-list">Pratima Shahi</span>
+                <span class="data-list">Man Shahi</span>
+                <span class="data-list">Ganesh Chand</span>
+                <span class="data-list">Bikash Chand</span>
+              </div>
+              <div class="data">
+                <span class="data-title">Email</span>
+                <span class="data-list">premshahi@gmail.com</span>
+                <span class="data-list">deepachand@gmail.com</span>
+                <span class="data-list">prakashhai@gmail.com</span>
+                <span class="data-list">manishachand@gmail.com</span>
+                <span class="data-list">pratimashhai@gmail.com</span>
+                <span class="data-list">manshahi@gmail.com</span>
+                <span class="data-list">ganeshchand@gmail.com</span>
+              </div>
+              <div class="data">
+                <span class="data-title">Joined</span>
+                <span class="data-list">2022-02-12</span>
+                <span class="data-list">2022-02-12</span>
+                <span class="data-list">2022-02-13</span>
+                <span class="data-list">2022-02-13</span>
+                <span class="data-list">2022-02-14</span>
+                <span class="data-list">2022-02-14</span>
+                <span class="data-list">2022-02-15</span>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
+      </section>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
-        crossorigin="anonymous"></script>
-</body>
-
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3"
+      crossorigin="anonymous"
+    ></script>
+  </body>
 </html>
