df6b80a4d7daa1300b9bf8467a87a0f13570c06d
Abderrahim Dahmani <80603216+AbdrrahimDahmani@users.noreply.github.com>
2023-06-28 01:14:55 +0100

    pregnancies upadted

--- PatientView/pregnancies.php
+++ PatientView/pregnancies.php
@@ -115,7 +115,6 @@
                                 $dueDateCountDown = $currentDateTime->diff($dueDateImmutable);
                                 //getting days
                                 $countdownDays = $dueDateCountDown->format('%R%a');
-                                echo $countdownDays;
                                 if ($countdownDays <= 0) {
                                     array_push($firstPregancyRow,$pregnancyRow);     
                                 } else {
@@ -163,7 +162,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="exampleModalLabel">Previous Pregnancies</h1>
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Add Pregnancie</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
